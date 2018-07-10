b5f49af29f6d9f1821517d9b81afd34353ee45ea
liulinjie <liulinjie1990823@gmail.com>
2018-07-10 14:36:31 +0800

    更新代码

--- app/src/main/java/com/llj/architecturedemo/ui/activity/ConstraintActivity.java
+++ app/src/main/java/com/llj/architecturedemo/ui/activity/ConstraintActivity.java
@@ -2,8 +2,8 @@ package com.llj.architecturedemo.ui.activity;
 
 import android.os.Bundle;
 
+import com.llj.architecturedemo.MyBaseActivity;
 import com.llj.architecturedemo.R;
-import com.llj.lib.base.MvpBaseActivity;
 
 /**
  * ArchitectureDemo
@@ -11,7 +11,7 @@ import com.llj.lib.base.MvpBaseActivity;
  * author liulj
  * date 2018/7/5
  */
-public class ConstraintActivity extends MvpBaseActivity{
+public class ConstraintActivity extends MyBaseActivity {
     @Override
     public int layoutId() {
         return R.layout.activity_constraint;
